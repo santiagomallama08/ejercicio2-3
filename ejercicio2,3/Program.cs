98b9b2300101821910569f478bf7e764c7c6c117
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 12:41:57 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio2,3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejercicio
+{
+    public class ejercicio
+    {
+        public static void Main()
+        {
+            int numero;
+            int resultado;
+            int doblado;
+            Console.WriteLine("por favor ingrese un numero ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            doblado = numero * 2;
+            resultado = doblado * 25;
+            Console.WriteLine($"el resultado de la operacion es {resultado}");
+        }
+    }
+}
